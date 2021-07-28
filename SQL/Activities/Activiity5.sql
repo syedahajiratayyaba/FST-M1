735829cc2a45a7fc8c530b417b5fb9b8f3131ba2
Syeda Hajira Tayyaba <45248882+syedahajiratayyaba@users.noreply.github.com>
2021-07-28 22:24:17 +0530

    Add files via upload

---
+++ SQL/Activities/Activiity5.sql
@@ -0,0 +1,17 @@
+REM   Script: Activity5
+REM   Activity5
+
+ALTER TABLE salesman ADD grade int;
+
+UPDATE salesman SET grade=100;
+
+SELECT * FROM salesman;
+
+UPDATE salesman SET grade=200 WHERE salesman_city='Rome';
+
+UPDATE salesman SET grade=300 WHERE salesman_name='James Hoog';
+
+UPDATE salesman SET salesman_name='Pierre' WHERE salesman_name='McLyon';
+
+SELECT * FROM salesman;
+
---
+++ SQL/Activities/Activity4.sql
@@ -0,0 +1,9 @@
+REM   Script: Activity4
+REM   Activity4
+
+ALTER TABLE salesman ADD grade int;
+
+UPDATE salesman SET grade=100;
+
+SELECT * FROM salesman;
+
